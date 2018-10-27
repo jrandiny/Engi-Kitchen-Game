8784bbd5ca402fa98e14b1f49a78a0ce46751767
sekarlm <38123936+sekarlm@users.noreply.github.com>
2018-10-27 23:58:31 +0700

    Update point.h

--- ADT/point/point.h
+++ ADT/point/point.h
@@ -1,5 +1,6 @@
 /* File: point.h */
-/* Tanggal: 28 Agustus 2016 */
+/* Tanggal: 26 Oktober 2018 */
+
 /* *** Definisi ABSTRACT DATA TYPE POINT *** */
 
 #ifndef POINT_H
@@ -13,16 +14,16 @@ typedef struct {
 } POINT;
 
 /* *** Notasi Akses: Selektor POINT *** */
-#define Absis(P) (P).X
-#define Ordinat(P) (P).Y
+#define P_Absis(P) (P).X
+#define P_Ordinat(P) (P).Y
 
 /* *** DEFINISI PROTOTIPE PRIMITIF *** */
 /* *** Konstruktor membentuk POINT *** */
-POINT MakePOINT (float X, float Y);
+POINT P_MakePOINT (float X, float Y);
 /* Membentuk sebuah POINT dari komponen-komponennya */
 
 /* *** KELOMPOK Interaksi dengan I/O device, BACA/TULIS  *** */
-void BacaPOINT (POINT * P);
+void P_BacaPOINT (POINT * P);
 /* Membaca nilai absis dan ordinat dari keyboard dan membentuk
    POINT P berdasarkan dari nilai absis dan ordinat tersebut */
 /* Komponen X dan Y dibaca dalam 1 baris, dipisahkan 1 buah spasi */
@@ -30,7 +31,7 @@ void BacaPOINT (POINT * P);
    akan membentuk POINT <1,2> */
 /* I.S. Sembarang */
 /* F.S. P terdefinisi */
-void TulisPOINT (POINT P);
+void P_TulisPOINT (POINT P);
 /* Nilai P ditulis ke layar dengan format "(X,Y)"
    tanpa spasi, enter, atau karakter lain di depan, belakang,
    atau di antaranya
@@ -40,59 +41,59 @@ void TulisPOINT (POINT P);
 /* F.S. P tertulis di layar dengan format "(X,Y)" */
 
 /* *** Kelompok operasi relasional terhadap POINT *** */
-boolean EQ (POINT P1, POINT P2);
+boolean P_EQ (POINT P1, POINT P2);
 /* Mengirimkan true jika P1 = P2 : absis dan ordinatnya sama */
-boolean NEQ (POINT P1, POINT P2);
+boolean P_NEQ (POINT P1, POINT P2);
 /* Mengirimkan true jika P1 tidak sama dengan P2 */
 
 /* *** Kelompok menentukan di mana P berada *** */
-boolean IsOrigin (POINT P);
+boolean P_IsOrigin (POINT P);
 /* Menghasilkan true jika P adalah titik origin */
-boolean IsOnSbX (POINT P);
+boolean P_IsOnSbX (POINT P);
 /* Menghasilkan true jika P terletak Pada sumbu X */
-boolean IsOnSbY (POINT P);
+boolean P_IsOnSbY (POINT P);
 /* Menghasilkan true jika P terletak pada sumbu Y */
-int Kuadran (POINT P);
+int P_Kuadran (POINT P);
 /* Menghasilkan kuadran dari P: 1, 2, 3, atau 4 */
 /* Prekondisi : P bukan titik origin, */
 /*              dan P tidak terletak di salah satu sumbu */
 
 /* *** KELOMPOK OPERASI LAIN TERHADAP TYPE *** */
-POINT NextX (POINT P);
+POINT P_NextX (POINT P);
 /* Mengirim salinan P dengan absis ditambah satu */
-POINT NextY (POINT P);
+POINT P_NextY (POINT P);
 /* Mengirim salinan P dengan ordinat ditambah satu */
-POINT PlusDelta (POINT P, float deltaX, float deltaY);
+POINT P_PlusDelta (POINT P, float deltaX, float deltaY);
 /* Mengirim salinan P yang absisnya adalah Absis(P) + deltaX dan ordinatnya adalah Ordinat(P) + deltaY */
-POINT MirrorOf (POINT P, boolean SbX);
+POINT P_MirrorOf (POINT P, boolean SbX);
 /* Menghasilkan salinan P yang dicerminkan terhadap salah satu sumbu */
 /* Jika SbX bernilai true, maka dicerminkan terhadap sumbu X */
 /* Jika SbX bernilai false, maka dicerminkan terhadap sumbu Y */
-float Jarak0 (POINT P);
+float P_Jarak0 (POINT P);
 /* Menghitung jarak P ke (0,0) */
-float Panjang (POINT P1, POINT P2);
+float P_Panjang (POINT P1, POINT P2);
 /* Menghitung panjang garis yang dibentuk P1 dan P2 */
 /* Perhatikanlah bahwa di sini spec fungsi kurang baik sebab menyangkut ADT Garis. */
 /* Tuliskan spec fungsi yang lebih tepat. */
-void Geser (POINT *P, float deltaX, float deltaY);
+void P_Geser (POINT *P, float deltaX, float deltaY);
 /* I.S. P terdefinisi */
 /* F.S. P digeser, absisnya sebesar deltaX dan ordinatnya sebesar deltaY */
-void GeserKeSbX (POINT *P);
+void P_GeserKeSbX (POINT *P);
 /* I.S. P terdefinisi */
 /* F.S. P berada pada sumbu X dengan absis sama dengan absis semula. */
 /* Proses : P digeser ke sumbu X. */
 /* Contoh : Jika koordinat semula (9,9), maka menjadi (9,0) */
-void GeserKeSbY (POINT *P);
+void P_GeserKeSbY (POINT *P);
 /* I.S. P terdefinisi*/
 /* F.S. P berada pada sumbu Y dengan ordinat yang sama dengan semula. */
 /* Proses : P digeser ke sumbu Y. */
 /* Contoh : Jika koordinat semula (9,9), maka menjadi (0,9) */
-void Mirror (POINT *P, boolean SbX);
+void P_Mirror (POINT *P, boolean SbX);
 /* I.S. P terdefinisi */
 /* F.S. P dicerminkan tergantung nilai SbX atau SbY */
 /* Jika SbX true maka dicerminkan terhadap sumbu X */
 /* Jika SbX false maka dicerminkan terhadap sumbu Y */
-void Putar (POINT *P, float Sudut);
+void P_Putar (POINT *P, float Sudut);
 /* I.S. P terdefinisi */
 /* F.S. P digeser sebesar Sudut derajat dengan sumbu titik (0,0) */
 
