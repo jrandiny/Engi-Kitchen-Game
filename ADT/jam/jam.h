a5988e537d42c458ae80ff8ecd4c0fb18be84295
sekarlm <38123936+sekarlm@users.noreply.github.com>
2018-10-27 23:56:32 +0700

    Update jam.h

--- ADT/jam/jam.h
+++ ADT/jam/jam.h
@@ -1,5 +1,5 @@
-/* File: jam.h */
-/* Tanggal: 28 Agustus 2016 */
+/* File	  : jam.h */
+/* Tanggal: 26 Oktober 2018 */
 /* Definisi ADT JAM */
 
 #ifndef JAM_H
@@ -15,28 +15,28 @@ typedef struct {
 } JAM;
 
 /* *** Notasi Akses: selektor JAM *** */
-#define Hour(J) (J).HH
-#define Minute(J) (J).MM
-#define Second(J) (J).SS
+#define J_Hour(J) (J).HH
+#define J_Minute(J) (J).MM
+#define J_Second(J) (J).SS
 
 /* ***************************************************************** */
 /* DEFINISI PRIMITIF                                                 */
 /* ***************************************************************** */
 /* KELOMPOK VALIDASI TERHADAP TYPE                                   */
 /* ***************************************************************** */
-boolean IsJAMValid (int H, int M, int S);
+boolean J_IsJAMValid (int H, int M, int S);
 /* Mengirim true  jika H,M,S dapat membentuk J yang valid */
 /* dipakai untuk mentest SEBELUM membentuk sebuah Jam */
 
 /* *** Konstruktor: Membentuk sebuah JAM dari komponen-komponennya *** */
-JAM MakeJAM (int HH, int MM, int SS);
+JAM J_MakeJAM (int HH, int MM, int SS);
 /* Membentuk sebuah JAM dari komponen-komponennya yang valid */
 /* Prekondisi : HH, MM, SS valid untuk membentuk JAM */
 
 /* ***************************************************************** */
 /* KELOMPOK BACA/TULIS                                               */
 /* ***************************************************************** */
-void BacaJAM (JAM * J);
+void J_BacaJAM (JAM * J);
 /* I.S. : J tidak terdefinisi */
 /* F.S. : J terdefinisi dan merupakan jam yang valid */
 /* Proses : mengulangi membaca komponen HH, MM, SS sehingga membentuk J */
@@ -51,7 +51,7 @@ void BacaJAM (JAM * J);
    1 3 4
    --> akan terbentuk JAM <1,3,4> */
 
-void TulisJAM (JAM J);
+void J_TulisJAM (JAM J);
 /* I.S. : J sembarang */
 /* F.S. :   Nilai J ditulis dg format HH:MM:SS */
 /* Proses : menulis nilai setiap komponen J ke layar dalam format HH:MM:SS
@@ -60,12 +60,12 @@ void TulisJAM (JAM J);
 /* ***************************************************************** */
 /* KELOMPOK KONVERSI TERHADAP TYPE                                   */
 /* ***************************************************************** */
-long JAMToDetik (JAM J);
+long J_JAMToDetik (JAM J);
 /* Diberikan sebuah JAM, mengkonversi menjadi jumlah detik dari pukul 0:0:0 */
 /* Rumus : detik = 3600*HH + 60*MM + SS */
 /* Nilai maksimum = 3600*23+59*60+59 */
 
-JAM DetikToJAM (long N);
+JAM J_DetikToJAM (long N);
 /* Mengirim  konversi detik ke JAM */
 /* Catatan: Jika N >= 86400, maka harus dikonversi dulu menjadi jumlah detik yang
    mewakili jumlah detik yang mungkin dalam 1 hari, yaitu dengan rumus:
@@ -75,25 +75,25 @@ JAM DetikToJAM (long N);
 /* KELOMPOK OPERASI TERHADAP TYPE                                    */
 /* ***************************************************************** */
 /* *** Kelompok Operator Relational *** */
-boolean JEQ (JAM J1, JAM J2);
+boolean J_JEQ (JAM J1, JAM J2);
 /* Mengirimkan true jika J1=J2, false jika tidak */
-boolean JNEQ (JAM J1, JAM J2);
+boolean J_JNEQ (JAM J1, JAM J2);
 /* Mengirimkan true jika J1 tidak sama dengan J2 */
-boolean JLT (JAM J1, JAM J2);
+boolean J_JLT (JAM J1, JAM J2);
 /* Mengirimkan true jika J1<J2, false jika tidak */
-boolean JGT (JAM J1, JAM J2);
+boolean J_JGT (JAM J1, JAM J2);
 /* Mengirimkan true jika J1>J2, false jika tidak */
 /* *** Operator aritmatika JAM *** */
-JAM NextDetik (JAM J);
+JAM J_NextDetik (JAM J);
 /* Mengirim 1 detik setelah J dalam bentuk JAM */
-JAM NextNDetik (JAM J, int N);
+JAM J_NextNDetik (JAM J, int N);
 /* Mengirim N detik setelah J dalam bentuk JAM */
-JAM PrevDetik (JAM J);
+JAM J_PrevDetik (JAM J);
 /* Mengirim 1 detik sebelum J dalam bentuk JAM */
-JAM PrevNDetik (JAM J, int N);
+JAM J_PrevNDetik (JAM J, int N);
 /* Mengirim N detik sebelum J dalam bentuk JAM */
 /* *** Kelompok Operator Aritmetika *** */
-long Durasi (JAM JAw, JAM JAkh);
+long J_Durasi (JAM JAw, JAM JAkh);
 /* Mengirim JAkh-JAw dlm Detik, dengan kalkulasi */
 /* Jika JAw > JAkh, maka JAkh adalah 1 hari setelah JAw */
 
