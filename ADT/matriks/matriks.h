5db5575215a9bcd5344a3b6cff90e9c002ccf4ad
sekarlm <38123936+sekarlm@users.noreply.github.com>
2018-10-28 00:00:56 +0700

    Update matriks.h

--- ADT/matriks/matriks.h
+++ ADT/matriks/matriks.h
@@ -24,40 +24,40 @@ typedef struct {
 
 /* ********** DEFINISI PROTOTIPE PRIMITIF ********** */
 /* *** Konstruktor membentuk MATRIKS *** */
-void MakeMATRIKS (int NB, int NK, MATRIKS * M);
+void M_MakeMATRIKS (int NB, int NK, MATRIKS * M);
 /* Membentuk sebuah MATRIKS "kosong" yang siap diisi berukuran NB x NK di "ujung kiri" memori */
 /* I.S. NB dan NK adalah valid untuk memori matriks yang dibuat */
 /* F.S. Matriks M sesuai dengan definisi di atas terbentuk */
 
 /* *** Selektor *** */
-#define NBrsEff(M) (M).NBrsEff
-#define NKolEff(M) (M).NKolEff
-#define Elmt(M,i,j) (M).Mem[(i)][(j)]
+#define M_NBrsEff(M) (M).NBrsEff
+#define M_NKolEff(M) (M).NKolEff
+#define M_Elmt(M,i,j) (M).Mem[(i)][(j)]
 
 /* *** Selektor "DUNIA MATRIKS" *** */
-boolean IsIdxValid (int i, int j);
+boolean M_IsIdxValid (int i, int j);
 /* Mengirimkan true jika i, j adalah indeks yang valid untuk matriks apa pun */
 
 /* *** Selektor: Untuk sebuah matriks M yang terdefinisi: *** */
-indeks GetFirstIdxBrs (MATRIKS M);
+indeks M_GetFirstIdxBrs (MATRIKS M);
 /* Mengirimkan indeks baris terkecil M */
-indeks GetFirstIdxKol (MATRIKS M);
+indeks M_GetFirstIdxKol (MATRIKS M);
 /* Mengirimkan indeks kolom terkecil M */
-indeks GetLastIdxBrs (MATRIKS M);
+indeks M_GetLastIdxBrs (MATRIKS M);
 /* Mengirimkan indeks baris terbesar M */
-indeks GetLastIdxKol (MATRIKS M);
+indeks M_GetLastIdxKol (MATRIKS M);
 /* Mengirimkan indeks kolom terbesar M */
-boolean IsIdxEff (MATRIKS M, indeks i, indeks j);
+boolean M_IsIdxEff (MATRIKS M, indeks i, indeks j);
 /* Mengirimkan true jika i, j adalah indeks efektif bagi M */
-ElType GetElmtDiagonal (MATRIKS M, indeks i);
+ElType M_GetElmtDiagonal (MATRIKS M, indeks i);
 /* Mengirimkan elemen M(i,i) */
 
 /* ********** Assignment  MATRIKS ********** */
-void CopyMATRIKS (MATRIKS MIn, MATRIKS * MHsl);
+void M_CopyMATRIKS (MATRIKS MIn, MATRIKS * MHsl);
 /* Melakukan assignment MHsl  MIn */
 
 /* ********** KELOMPOK BACA/TULIS ********** */
-void BacaMATRIKS (MATRIKS * M, int NB, int NK);
+void M_BacaMATRIKS (MATRIKS * M, int NB, int NK);
 /* I.S. IsIdxValid(NB,NK) */
 /* F.S. M terdefinisi nilai elemen efektifnya, berukuran NB x NK */
 /* Proses: Melakukan MakeMATRIKS(M,NB,NK) dan mengisi nilai efektifnya */
@@ -67,7 +67,7 @@ void BacaMATRIKS (MATRIKS * M, int NB, int NK);
 4 5 6
 8 9 10
 */
-void TulisMATRIKS (MATRIKS M);
+void M_TulisMATRIKS (MATRIKS M);
 /* I.S. M terdefinisi */
 /* F.S. Nilai M(i,j) ditulis ke layar per baris per kolom, masing-masing elemen per baris
    dipisahkan sebuah spasi */
@@ -79,58 +79,58 @@ void TulisMATRIKS (MATRIKS M);
 */
 
 /* ********** KELOMPOK OPERASI ARITMATIKA TERHADAP TYPE ********** */
-MATRIKS TambahMATRIKS (MATRIKS M1, MATRIKS M2);
+MATRIKS M_TambahMATRIKS (MATRIKS M1, MATRIKS M2);
 /* Prekondisi : M1  berukuran sama dengan M2 */
 /* Mengirim hasil penjumlahan matriks: M1 + M2 */
-MATRIKS KurangMATRIKS (MATRIKS M1, MATRIKS M2);
+MATRIKS M_KurangMATRIKS (MATRIKS M1, MATRIKS M2);
 /* Prekondisi : M berukuran sama dengan M */
 /* Mengirim hasil pengurangan matriks: salinan M1 – M2 */
-MATRIKS KaliMATRIKS (MATRIKS M1, MATRIKS M2);
+MATRIKS M_KaliMATRIKS (MATRIKS M1, MATRIKS M2);
 /* Prekondisi : Ukuran kolom efektif M1 = ukuran baris efektif M2 */
 /* Mengirim hasil perkalian matriks: salinan M1 * M2 */
-MATRIKS KaliKons (MATRIKS M, ElType X);
+MATRIKS M_KaliKons (MATRIKS M, ElType X);
 /* Mengirim hasil perkalian setiap elemen M dengan X */
-void PKaliKons (MATRIKS * M, ElType K);
+void M_PKaliKons (MATRIKS * M, ElType K);
 /* I.S. M terdefinisi, K terdefinisi */
 /* F.S. Mengalikan setiap elemen M dengan K */
 
 /* ********** KELOMPOK OPERASI RELASIONAL TERHADAP MATRIKS ********** */
-boolean EQ (MATRIKS M1, MATRIKS M2);
+boolean M_EQ (MATRIKS M1, MATRIKS M2);
 /* Mengirimkan true jika M1 = M2, yaitu NBElmt(M1) = NBElmt(M2) dan */
 /* untuk setiap i,j yang merupakan indeks baris dan kolom M1(i,j) = M2(i,j) */
 /* Juga merupakan strong EQ karena GetFirstIdxBrs(M1) = GetFirstIdxBrs(M2)
    dan GetLastIdxKol(M1) = GetLastIdxKol(M2) */
-boolean NEQ (MATRIKS M1, MATRIKS M2);
+boolean M_NEQ (MATRIKS M1, MATRIKS M2);
 /* Mengirimkan true jika M1 tidak sama dengan M2 */
-boolean EQSize (MATRIKS M1, MATRIKS M2);
+boolean M_EQSize (MATRIKS M1, MATRIKS M2);
 /* Mengirimkan true jika ukuran efektif matriks M1 sama dengan ukuran efektif M2 */
 /* yaitu GetBrsEff(M1) = GetNBrsEff (M2) dan GetNKolEff (M1) = GetNKolEff (M2) */
 
 /* ********** Operasi lain ********** */
-int NBElmt (MATRIKS M);
+int M_NBElmt (MATRIKS M);
 /* Mengirimkan banyaknya elemen M */
 
 /* ********** KELOMPOK TEST TERHADAP MATRIKS ********** */
-boolean IsBujurSangkar (MATRIKS M);
+boolean M_IsBujurSangkar (MATRIKS M);
 /* Mengirimkan true jika M adalah matriks dg ukuran baris dan kolom sama */
-boolean IsSimetri (MATRIKS M);
+boolean M_IsSimetri (MATRIKS M);
 /* Mengirimkan true jika M adalah matriks simetri : IsBujurSangkar(M)
    dan untuk setiap elemen M, M(i,j)=M(j,i) */
-boolean IsSatuan (MATRIKS M);
+boolean M_IsSatuan (MATRIKS M);
 /* Mengirimkan true jika M adalah matriks satuan: IsBujurSangkar(M) dan
    setiap elemen diagonal M bernilai 1 dan elemen yang bukan diagonal bernilai 0 */
-boolean IsSparse (MATRIKS M);
+boolean M_IsSparse (MATRIKS M);
 /* Mengirimkan true jika M adalah matriks sparse: mariks “jarang” dengan definisi:
    hanya maksimal 5% dari memori matriks yang efektif bukan bernilai 0 */
-MATRIKS Inverse1 (MATRIKS M);
+MATRIKS M_Inverse1 (MATRIKS M);
 /* Menghasilkan salinan M dengan setiap elemen "di-invers", yaitu dinegasikan (dikalikan -1) */
-float Determinan (MATRIKS M);
+float M_Determinan (MATRIKS M);
 /* Prekondisi: IsBujurSangkar(M) */
 /* Menghitung nilai determinan M */
-void PInverse1 (MATRIKS * M);
+void M_PInverse1 (MATRIKS * M);
 /* I.S. M terdefinisi */
 /* F.S. M di-invers, yaitu setiap elemennya dinegasikan (dikalikan -1) */
-void Transpose (MATRIKS * M);
+void M_Transpose (MATRIKS * M);
 /* I.S. M terdefinisi dan IsBujursangkar(M) */
 /* F.S. M "di-transpose", yaitu setiap elemen M(i,j) ditukar nilainya dengan elemen M(j,i) */
 
