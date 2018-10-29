93e0ebb4dc75a662b4a0be59d657e7692d11d027
jrandiny <jrandiny2@gmail.com>
2018-10-29 22:04:04 +0700

    Modify POINT to Point (standarization of case)

--- ADT/point/point.c
+++ ADT/point/point.c
@@ -2,18 +2,18 @@
    Nama file : point.c
    Topik     : Engi's Kitchen Game
    Tanggal   : 26-10-2018
-   Deskripsi : ADT untuk tipe POINT*/
-/* *** Definisi ABSTRACT DATA TYPE POINT *** */
+   Deskripsi : ADT untuk tipe Point*/
+/* *** Definisi ABSTRACT DATA TYPE Point *** */
 
 #include "point.h"
 
 /* *** DEFINISI PROTOTIPE PRIMITIF *** */
-/* *** Konstruktor membentuk POINT *** */
-POINT P_MakePOINT (int X, int Y)
-/* Membentuk sebuah POINT dari komponen-komponennya */
+/* *** Konstruktor membentuk Point *** */
+Point P_MakePoint (int X, int Y)
+/* Membentuk sebuah Point dari komponen-komponennya */
 {
   /* KAMUS */
-  POINT P;
+  Point P;
 
   /* ALGORITMA */
   P_Absis(P) = X;
@@ -23,12 +23,12 @@ POINT P_MakePOINT (int X, int Y)
 }
 
 /* *** KELOMPOK Interaksi dengan I/O device, BACA/TULIS  *** */
-void P_BacaPOINT (POINT * P)
+void P_BacaPoint (Point * P)
 /* Membaca nilai absis dan ordinat dari keyboard dan membentuk
-   POINT P berdasarkan dari nilai absis dan ordinat tersebut */
+   Point P berdasarkan dari nilai absis dan ordinat tersebut */
 /* Komponen X dan Y dibaca dalam 1 baris, dipisahkan 1 buah spasi */
 /* Contoh: 1 2
-   akan membentuk POINT <1,2> */
+   akan membentuk Point <1,2> */
 /* I.S. Sembarang */
 /* F.S. P terdefinisi */
 {
@@ -37,10 +37,10 @@ void P_BacaPOINT (POINT * P)
 
   /* ALGORITMA */
   scanf("%f %f",&X,&Y);
-  *P = P_MakePOINT(X,Y);
+  *P = P_MakePoint(X,Y);
 }
 
-void P_TulisPOINT (POINT P)
+void P_TulisPoint (Point P)
 /* Nilai P ditulis ke layar dengan format "(X,Y)"
    tanpa spasi, enter, atau karakter lain di depan, belakang,
    atau di antaranya
@@ -55,13 +55,13 @@ void P_TulisPOINT (POINT P)
   printf("(%d,%d)",P_Absis(P),P_Ordinat(P));
 }
 
-/* *** Kelompok operasi relasional terhadap POINT *** */
-boolean P_EQ (POINT P1, POINT P2)
+/* *** Kelompok operasi relasional terhadap Point *** */
+boolean P_EQ (Point P1, Point P2)
 /* Mengirimkan true jika P1 = P2 : absis dan ordinatnya sama */
 {
   return ((P_Absis(P1)==P_Absis(P2)) && (P_Ordinat(P1)==P_Ordinat(P2)));
 }
-boolean P_NEQ (POINT P1, POINT P2)
+boolean P_NEQ (Point P1, Point P2)
 /* Mengirimkan true jika P1 tidak sama dengan P2 */
 {
   return ((P_Absis(P1)!=P_Absis(P2)) && (P_Ordinat(P1)!=P_Ordinat(P2)));
@@ -69,17 +69,17 @@ boolean P_NEQ (POINT P1, POINT P2)
 
 
 /* *** KELOMPOK OPERASI LAIN TERHADAP TYPE *** */
-POINT P_NextX (POINT P)
+Point P_NextX (Point P)
 /* Mengirim salinan P dengan absis ditambah satu */
 {
-  return P_MakePOINT((P_Absis(P)+1), P_Ordinat(P));
+  return P_MakePoint((P_Absis(P)+1), P_Ordinat(P));
 }
-POINT P_NextY (POINT P)
+Point P_NextY (Point P)
 /* Mengirim salinan P dengan ordinat ditambah satu */
 {
-  return P_MakePOINT(P_Absis(P), (P_Ordinat(P)+1));
+  return P_MakePoint(P_Absis(P), (P_Ordinat(P)+1));
 }
-POINT P_PlusDelta (POINT P, int deltaX, int deltaY)
+Point P_PlusDelta (Point P, int deltaX, int deltaY)
 /* Mengirim salinan P yang absisnya adalah Absis(P) + deltaX dan ordinatnya adalah Ordinat(P) + deltaY */
 {
   /* KAMUS */
@@ -89,10 +89,10 @@ POINT P_PlusDelta (POINT P, int deltaX, int deltaY)
   newX = P_Absis(P) + deltaX;
   newY = P_Ordinat(P) + deltaY;
 
-  return P_MakePOINT(newX,newY);
+  return P_MakePoint(newX,newY);
 }
 
-void P_Geser (POINT *P, int deltaX, int deltaY)
+void P_Geser (Point *P, int deltaX, int deltaY)
 /* I.S. P terdefinisi */
 /* F.S. P digeser, absisnya sebesar deltaX dan ordinatnya sebesar deltaY */
 {
--- ADT/point/point.h
+++ ADT/point/point.h
@@ -1,37 +1,37 @@
 /* File: point.h */
 /* Tanggal: 26 Oktober 2018 */
 
-/* *** Definisi ABSTRACT DATA TYPE POINT *** */
+/* *** Definisi ABSTRACT DATA TYPE Point *** */
 
-#ifndef POINT_H
-#define POINT_H
+#ifndef Point_H
+#define Point_H
 
 #include "../../header/std.h"
 
 typedef struct {
 	int X; /* absis   */
 	int Y; /* ordinat */
-} POINT;
+} Point;
 
-/* *** Notasi Akses: Selektor POINT *** */
+/* *** Notasi Akses: Selektor Point *** */
 #define P_Absis(P) (P).X
 #define P_Ordinat(P) (P).Y
 
 /* *** DEFINISI PROTOTIPE PRIMITIF *** */
-/* *** Konstruktor membentuk POINT *** */
-POINT P_MakePOINT (int X, int Y);
-/* Membentuk sebuah POINT dari komponen-komponennya */
+/* *** Konstruktor membentuk Point *** */
+Point P_MakePoint (int X, int Y);
+/* Membentuk sebuah Point dari komponen-komponennya */
 
 /* *** KELOMPOK Interaksi dengan I/O device, BACA/TULIS  *** */
-void P_BacaPOINT (POINT * P);
+void P_BacaPoint (Point * P);
 /* Membaca nilai absis dan ordinat dari keyboard dan membentuk
-   POINT P berdasarkan dari nilai absis dan ordinat tersebut */
+   Point P berdasarkan dari nilai absis dan ordinat tersebut */
 /* Komponen X dan Y dibaca dalam 1 baris, dipisahkan 1 buah spasi */
 /* Contoh: 1 2
-   akan membentuk POINT <1,2> */
+   akan membentuk Point <1,2> */
 /* I.S. Sembarang */
 /* F.S. P terdefinisi */
-void P_TulisPOINT (POINT P);
+void P_TulisPoint (Point P);
 /* Nilai P ditulis ke layar dengan format "(X,Y)"
    tanpa spasi, enter, atau karakter lain di depan, belakang,
    atau di antaranya
@@ -40,21 +40,21 @@ void P_TulisPOINT (POINT P);
 /* I.S. P terdefinisi */
 /* F.S. P tertulis di layar dengan format "(X,Y)" */
 
-/* *** Kelompok operasi relasional terhadap POINT *** */
-boolean P_EQ (POINT P1, POINT P2);
+/* *** Kelompok operasi relasional terhadap Point *** */
+boolean P_EQ (Point P1, Point P2);
 /* Mengirimkan true jika P1 = P2 : absis dan ordinatnya sama */
-boolean P_NEQ (POINT P1, POINT P2);
+boolean P_NEQ (Point P1, Point P2);
 /* Mengirimkan true jika P1 tidak sama dengan P2 */
 
 /* *** KELOMPOK OPERASI LAIN TERHADAP TYPE *** */
-POINT P_NextX (POINT P);
+Point P_NextX (Point P);
 /* Mengirim salinan P dengan absis ditambah satu */
-POINT P_NextY (POINT P);
+Point P_NextY (Point P);
 /* Mengirim salinan P dengan ordinat ditambah satu */
-POINT P_PlusDelta (POINT P, int deltaX, int deltaY);
+Point P_PlusDelta (Point P, int deltaX, int deltaY);
 /* Mengirim salinan P yang absisnya adalah Absis(P) + deltaX dan ordinatnya adalah Ordinat(P) + deltaY */
 
-void P_Geser (POINT *P, int deltaX, int deltaY);
+void P_Geser (Point *P, int deltaX, int deltaY);
 /* I.S. P terdefinisi */
 /* F.S. P digeser, absisnya sebesar deltaX dan ordinatnya sebesar deltaY */
 
