fd96c6fd8a45a2e0e7d6a65a30a71223188dd0f5
jrandiny <jrandiny2@gmail.com>
2018-10-30 22:21:55 +0700

    Add simple ui test

---
+++ ADT Lain/UI/UItest.c
@@ -0,0 +1,30 @@
+#include "UI.h"
+#include <ncurses.h>
+#include <unistd.h>
+
+#include "../../ADT/point/point.h"
+#include "../../ADT/mesinkata/mesinkata.h"
+
+int main(){
+  GameScreen gs;
+  InitScreen(&gs);
+
+  Kata abc,up,orig3;
+
+  up = K_MakeKata("GU");
+
+  wrefresh(Hand_Panel(gs));
+
+
+  while(true){
+    RefreshTopPanel(&gs,orig3,1,2,3);
+    abc  = GetInput(&gs);
+
+    if(K_IsKataSama(abc, up)){
+      break;
+    }
+
+  }
+
+  endwin();
+}
