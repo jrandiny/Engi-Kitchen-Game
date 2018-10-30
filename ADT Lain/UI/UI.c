9bbc96eb4f6d60beb336ec87218267879f3d7e01
jrandiny <jrandiny2@gmail.com>
2018-10-30 19:33:34 +0700

    Update UI ADT with windowing

--- ADT Lain/UI/UI.c
+++ ADT Lain/UI/UI.c
@@ -2,11 +2,91 @@
 #include <ncurses.h>
 #include "../../ADT/point/point.h"
 
-void initScreen(){
-  initscr();
+
+void draw_borders(WINDOW *screen) {
+  int x, y, i;
+
+  getmaxyx(screen, y, x);
+
+  // 4 corners
+  mvwprintw(screen, 0, 0, "+");
+  mvwprintw(screen, y - 1, 0, "+");
+  mvwprintw(screen, 0, x - 1, "+");
+  mvwprintw(screen, y - 1, x - 1, "+");
+
+  // sides
+  for (i = 1; i < (y - 1); i++) {
+    mvwprintw(screen, i, 0, "|");
+    mvwprintw(screen, i, x - 1, "|");
+  }
+
+  // top and bottom
+  for (i = 1; i < (x - 1); i++) {
+    mvwprintw(screen, 0, i, "-");
+    mvwprintw(screen, y - 1, i, "-");
+  }
 }
 
-void drawRectangle(Point orig, int width, int height){
+void InitScreen(GameScreen *gs){
+  /* KAMUS LOKAL */
+  int parentX,parentY;
+  int sidePanelWidth;
+  int topBarHeight;
+  int sidePanelHeight;
+  int mainPanelWidth;
+
+  /* ALGORITMA */
+  initscr();
+  getmaxyx(stdscr, parentY, parentX);
+
+  sidePanelWidth = 20;
+  topBarHeight = 3;
+
+  mainPanelWidth = 48;
+
+  sidePanelHeight = 12;
+
+  Top_1_Panel(*gs) = newwin(topBarHeight,sidePanelWidth,0,0);
+  Top_2_Panel(*gs) = newwin(topBarHeight,sidePanelWidth,0,sidePanelWidth);
+  Top_3_Panel(*gs) = newwin(topBarHeight, mainPanelWidth-sidePanelWidth, 0,2*sidePanelWidth);
+  Top_4_Panel(*gs) = newwin(topBarHeight, sidePanelWidth,0,sidePanelWidth+mainPanelWidth);
+
+  Waiting_Panel(*gs) = newwin(sidePanelHeight,sidePanelWidth,topBarHeight,0);
+  Food_Panel(*gs) = newwin(sidePanelHeight, sidePanelWidth, topBarHeight, sidePanelWidth+mainPanelWidth);
+  Order_Panel(*gs) = newwin(sidePanelHeight,sidePanelWidth,topBarHeight+sidePanelHeight,0);
+  Hand_Panel(*gs) = newwin(sidePanelHeight,sidePanelWidth, topBarHeight+sidePanelHeight, sidePanelWidth+mainPanelWidth);
+
+  Command_Panel(*gs) = newwin(topBarHeight, sidePanelWidth*2 + mainPanelWidth, topBarHeight+2*sidePanelHeight, 0);
+
+  Main_Panel(*gs) = newwin(2*sidePanelHeight, mainPanelWidth,topBarHeight, sidePanelWidth);
+
+  draw_borders(Top_1_Panel(*gs));
+  draw_borders(Top_2_Panel(*gs));
+  draw_borders(Top_3_Panel(*gs));
+  draw_borders(Top_4_Panel(*gs));
+
+  draw_borders(Waiting_Panel(*gs));
+  draw_borders(Food_Panel(*gs));
+  draw_borders(Order_Panel(*gs));
+  draw_borders(Hand_Panel(*gs));
+  draw_borders(Command_Panel(*gs));
+  draw_borders(Main_Panel(*gs));
+
+  wrefresh(Main_Panel(*gs));
+  wrefresh(Top_1_Panel(*gs));
+  wrefresh(Top_2_Panel(*gs));
+  wrefresh(Top_3_Panel(*gs));
+  wrefresh(Top_4_Panel(*gs));
+  wrefresh(Command_Panel(*gs));
+  wrefresh(Waiting_Panel(*gs));
+  wrefresh(Hand_Panel(*gs));
+  wrefresh(Order_Panel(*gs));
+  wrefresh(Food_Panel(*gs));
+}
+
+
+
+void DrawRectangle(Point orig, int width, int height){
   BorderType standardBorder;
 
   UL(standardBorder) = ACS_ULCORNER;
@@ -14,11 +94,11 @@ void drawRectangle(Point orig, int width, int height){
   LL(standardBorder) = ACS_LLCORNER;
   LR(standardBorder) = ACS_LRCORNER;
 
-  drawRectangleCC(orig, width, height, standardBorder);
+  DrawRectangleCC(orig, width, height, standardBorder);
 
 }
 
-void drawRectangleCC(Point orig, int width, int height, BorderType border){
+void DrawRectangleCC(Point orig, int width, int height, BorderType border){
   /* KAMUS LOKAL */
   int i,j;
 
--- ADT Lain/UI/UI.h
+++ ADT Lain/UI/UI.h
@@ -6,7 +6,36 @@
 #include "../../ADT/matriks/matriks.h"
 #include "../../ADT/queue/queue.h"
 #include "../../ADT/stack/stackt.h"
-#include "../../ADT/listlinier/listlinier.h"
+//#include "../../ADT/listlinier/listlinier.h"
+#include <ncurses.h>
+
+/* Definisi GameScreen */
+typedef struct{
+  WINDOW *mainPanel;
+  WINDOW *topPanel1;
+  WINDOW *topPanel2;
+  WINDOW *topPanel3;
+  WINDOW *topPanel4;
+  WINDOW *commandPanel;
+  WINDOW *waitingPanel;
+  WINDOW *handPanel;
+  WINDOW *bottomPanel;
+  WINDOW *orderPanel;
+  WINDOW *foodPanel;
+}GameScreen;
+
+/* Notasi akses: Selektor GameScreen */
+#define Main_Panel(GS) (GS).mainPanel
+#define Top_1_Panel(GS) (GS).topPanel1
+#define Top_2_Panel(GS) (GS).topPanel2
+#define Top_3_Panel(GS) (GS).topPanel3
+#define Top_4_Panel(GS) (GS).topPanel4
+#define Command_Panel(GS) (GS).commandPanel
+#define Waiting_Panel(GS) (GS).waitingPanel
+#define Hand_Panel(GS) (GS).handPanel
+#define Order_Panel(GS) (GS).orderPanel
+#define Food_Panel(GS) (GS).foodPanel
+
 
 
 /* Definisi BorderType untuk gambar persegi panjang */
@@ -23,66 +52,59 @@ typedef struct {
 #define LL(B) (B).LL
 #define LR(B) (B).LR
 
-void initScreen();
+
+void InitScreen(GameScreen *gs);
 /* I.S. : Bebas */
-/* F.S. : Terinisialisai layar kosong */
+/* F.S. : Terinisialisai layar kosong dengan window diset ke gs */
+
+void QuitScreen(GameScreen *gs);
+/* I.S. : gs Terinisialisai */
+/* F.S. : semua gs tertutup*/
 
 /* KEOMPOK GAMBAR */
-void drawRectangle(Point orig, int width, int height);
+void DrawRectangle(Point orig, int width, int height);
 /* I.S. : Bebas sudah initScreen */
 /* F.S. : Digambar persegi denga origin point (kiri atas) pada orig
           lebar width, tinggi height, border standar (kotak)*/
 
-void drawRectangleCC(Point orig, int width, int height, BorderType border);
+void DrawRectangleCC(Point orig, int width, int height, BorderType border);
 /* I.S. : Bebas sudah initScreen */
 /* F.S. : Digambar persegi denga origin point (kiri atas) pada orig
           lebar width, tinggi height, border bisa diubah */
 
-void drawWaiter(Point waiter);
+/* Kelompok Refresh*/
+void RefreshWaiter(GameScreen *gs, Point waiter);
 /* I.S. : Bebas sudah initScreen */
 /* F.S. : Digambar sebuah pelayan pada posisi tertentu di layar */
 
-void drawMap(Matriks peta);
+void RefreshMap(GameScreen *gs, Matriks peta);
 /* I.S. : Bebas sudah initScreen */
 /* F.S. : Digambar peta kosong */
 
-void drawWaitingPanel(Queue waitQueue);
+void RefreshWaitingPanel(GameScreen *gs, Queue waitQueue);
 /* I.S. : Bebas sudah initScreen */
 /* F.S. : Digambar panel daftar pelanggan menunggu */
 
-void drawOrderPanel(List orderList);
+//void DrawOrderPanel(GameScreen *gs, List orderList);
 /* I.S. : Bebas sudah initScreen */
 /* F.S. : Digambar panel daftar order */
 
-void drawFoodPanel(Stack foodStack);
+void RefreshFoodPanel(GameScreen *gs, Stack foodStack);
 /* I.S. : Bebas sudah initScreen */
 /* F.S. : Digambar panel stack makanan */
 
-void drawHandPanel(Stack handStack);
+void RefreshHandPanel(GameScreen *gs, Stack handStack);
 /* I.S. : Bebas sudah initScreen */
 /* F.S. : Digambar panel stack di tangan */
 
-void drawTopPanel(Kata name, int money, int life, int time);
+void RefeshTopPanel(GameScreen *gs, Kata name, int money, int life, int time);
 /* I.S. : Bebas sudah initScreen */
 /* F.S. : Digambar panel atas */
 
-void drawBottomPanel();
-/* I.S. : Bebas sudah initScreen */
-/* F.S. : Digambar panel bawah*/
-
 /* KELOMPOK INPUT */
-Kata getInput();
+Kata GetInput(GameScreen *gs);
 /* I.S. : Bebas sudah initScreen */
 /* F.S. : Mengembalikan input user (command), mengembalikan ARROW jika
           yang ditekan adalah arrow key */
-
-Kata ARROW();
-/* I,S  : Bebas */
-/* F.S. : Mengembalikan kata ARROR, untuk mengecek apakah input merupakan
-          arrow key*/
-
-
-
-
 
 #endif
