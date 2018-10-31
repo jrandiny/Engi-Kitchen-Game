c822779d16fcc5e2aadd9504551f7dc653d39a4b
jrandiny <jrandiny2@gmail.com>
2018-10-31 18:15:53 +0700

    Add arraykata driver

---
+++ ADT/arraykata/marraykata.c
@@ -0,0 +1,77 @@
+/* Kelompok  : UAS
+   Nama file : marraykata.h
+   Topik     : Engi's Kitchen Game
+   Tanggal   : 31 Oktober 2018
+   Deskripsi : driver untuk ADT untuk tipe array kata*/
+
+#include "arraykata.h"
+#include <stdio.h>
+
+Kata K_MakeKata(char* S);
+/* Membuat kata dari string */
+
+void PrintArrKata(ArrKata ak);
+/* I.S. : ak terdefinisi */
+/* F.S. : Tercetak arr kata dalam 1 baris, jika kosong tercetak "Kosong" */
+
+
+int main(){
+  /* KAMUS */
+  ArrKata ak;
+  Kata input;
+  int pil;
+  char temp[50];
+
+  /* ALGORITMA */
+  AK_CreateEmpty(&ak);
+  do {
+    PrintArrKata(ak);
+    printf("1)Add kata\n2)Length\n3)Exit\nChoice = ");
+    scanf("%d",&pil);
+    if(pil==1){
+      scanf("%s",temp);
+      AK_AddAsLastEl(&ak, K_MakeKata(temp));
+    }else if(pil==2){
+      printf("Length = %d\n",AK_NbElmt(ak));
+    }
+  } while(pil!=3);
+
+  return 0;
+}
+
+/* Realisasi fungsi prosedur */
+Kata K_MakeKata(char* S)
+/* Membuat kata dari string */
+{
+  Kata hasil;
+  int i=0;
+  while(S[i]!='\0'){
+    hasil.TabKata[i+1]=S[i];
+    i++;
+  }
+  hasil.Length=i;
+  return hasil;
+}
+
+void PrintArrKata(ArrKata ak)
+/* I.S. : ak terdefinisi */
+/* F.S. : Tercetak arr kata dalam 1 baris, jika kosong tercetak "Kosong" */
+{
+  /* KAMUS LOKAL */
+  int i,j;
+
+  /* ALGORITMA */
+  if(AK_IsEmpty(ak)){
+    printf("Kosong");
+  }else{
+    for(i=1;i<=AK_Neff(ak);i++){
+      for(j=1;j<=AK_Elmt(ak,i).Length;j++){
+        printf("%c",AK_Elmt(ak, i).TabKata[j]);
+      }
+      printf(" ");
+    }
+  }
+
+  printf("\n");
+
+}
