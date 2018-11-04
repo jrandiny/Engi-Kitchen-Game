5713f7acd19e34ec2dd7e6a56744b1e0cc14de9a
a-johanes <antoniusjohanes99@gmail.com>
2018-11-04 23:19:00 +0700

    add stacktfood.c in compile.sh

--- compile.sh
+++ compile.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-gcc -o main main.c ADT/point/point.c ADT/space/space.c ADT/space/arrMeja/arrMeja.c ADT/space/matTile/matTile.c Modul/UI/UI.c Modul/eksternal/eksternal.c ADT/mesinkata/mesinkata.c ADT/mesinkata/mesinkar.c ADT/mesinkata/arrKata/arrKata.c ADT/space/grafRuangan/grafRuangan.c ADT/customer/prioqueuecustomer.c -lncursesw
+gcc -o main main.c ADT/food/stacktfood.c ADT/point/point.c ADT/space/space.c ADT/space/arrMeja/arrMeja.c ADT/space/matTile/matTile.c Modul/UI/UI.c Modul/eksternal/eksternal.c ADT/mesinkata/mesinkata.c ADT/mesinkata/mesinkar.c ADT/mesinkata/arrKata/arrKata.c ADT/space/grafRuangan/grafRuangan.c ADT/customer/prioqueuecustomer.c -lncursesw
 ./main
