6bcfedd6da7ee884fd069956b5a24b3a2ee83e4a
jrandiny <jrandiny2@gmail.com>
2018-11-23 15:42:23 +0700

    Update run.sh for new jam

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 mkdir -p bin
 case $1 in
 main)
-	gcc -Wall -o main main.c ADT/food/stackFood/stacktfood.c ADT/point/point.c ADT/arrInt/arrInt.c ADT/order/order.c ADT/order/arrOrder/arrOrder.c ADT/food/food.c ADT/food/treeFood/treeFood.c ADT/space/space.c ADT/space/arrMeja/arrMeja.c ADT/space/matTile/matTile.c Modul/UI/UI.c Modul/eksternal/eksternal.c ADT/mesinkata/mesinkata.c ADT/mesinkata/mesinkar.c ADT/mesinkata/arrKata/arrKata.c ADT/space/grafRuangan/grafRuangan.c ADT/customer/prioQueueCustomer.c ADT/jam/jam.c -lncursesw
+	gcc -Wall -o main main.c ADT/food/stackFood/stacktfood.c ADT/point/point.c ADT/arrInt/arrInt.c ADT/order/order.c ADT/order/arrOrder/arrOrder.c ADT/food/food.c ADT/food/treeFood/treeFood.c ADT/space/space.c ADT/space/arrMeja/arrMeja.c ADT/space/matTile/matTile.c Modul/UI/UI.c Modul/eksternal/eksternal.c ADT/mesinkata/mesinkata.c ADT/mesinkata/mesinkar.c ADT/mesinkata/arrKata/arrKata.c ADT/space/grafRuangan/grafRuangan.c ADT/customer/prioQueueCustomer.c -lncursesw
   mv main bin/main
   ./bin/main
 	;;
